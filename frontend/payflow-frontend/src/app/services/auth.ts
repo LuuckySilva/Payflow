b471c6ce9fea414eb656e60ff83f5c6d2a5fd056
Lucas <lukasantonyo@gmail.com>
2026-06-02 10:12:33 -0300

    feat: atualiza URLs da API para produção no Railway

--- frontend/payflow-frontend/src/app/services/auth.ts
+++ frontend/payflow-frontend/src/app/services/auth.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http'
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth'
+ private apiUrl = 'https://payflow-production-d05e.up.railway.app/api/auth'
 
   constructor(private http: HttpClient) {}
 
